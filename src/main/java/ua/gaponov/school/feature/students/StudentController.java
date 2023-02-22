9e88c5f45579b6370ff6089e0bd4a4642d0d2fc5
gaponov <wmconstructor@gmail.com>
2023-02-22 19:08:20 +0200

    refactoring SchoolClassController

--- src/main/java/ua/gaponov/school/feature/students/StudentController.java
+++ src/main/java/ua/gaponov/school/feature/students/StudentController.java
@@ -1,6 +1,5 @@
 package ua.gaponov.school.feature.students;
 
-import static ua.gaponov.school.utils.UrlUtils.SCHOOL_CLASS_URL;
 import static ua.gaponov.school.utils.UrlUtils.STUDENT_URL;
 
 import java.util.Comparator;
@@ -18,10 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import ua.gaponov.school.exception.SchoolClassNotFoundException;
 import ua.gaponov.school.exception.StudentNotFoundException;
-import ua.gaponov.school.feature.school.School;
-import ua.gaponov.school.feature.school.SchoolService;
 import ua.gaponov.school.feature.schoolclass.SchoolClass;
-import ua.gaponov.school.feature.schoolclass.SchoolClassDto;
 import ua.gaponov.school.feature.schoolclass.SchoolClassService;
 
 @RequiredArgsConstructor
@@ -46,13 +42,13 @@ public class StudentController {
   @PostMapping("/add")
   public RedirectView add(@RequestParam(value = "firstName") String firstName,
       @RequestParam(value = "lastName") String lastName,
-      @RequestParam(value = "class_id") int class_id,
+      @RequestParam(value = "class_id") int classId,
       @RequestParam(value = "return_path", required = false) String returnPath) {
 
     Student student = null;
     SchoolClass schoolClass = null;
     try {
-      schoolClass = schoolClassService.findById(class_id);
+      schoolClass = schoolClassService.findById(classId);
       student = Student.builder()
           .firstName(firstName)
           .lastName(lastName)
@@ -60,7 +56,7 @@ public class StudentController {
           .build();
       studentService.save(student);
     } catch (NotFoundException e) {
-      throw new SchoolClassNotFoundException("School class not found with id: " + class_id);
+      throw new SchoolClassNotFoundException("School class not found with id: " + classId);
     }
 
     if (returnPath == null) {
@@ -91,12 +87,12 @@ public class StudentController {
   public RedirectView editSchoolClass(@RequestParam(value = "id") int id,
       @RequestParam(value = "firstName") String firstName,
       @RequestParam(value = "lastName") String lastName,
-      @RequestParam(value = "class_id") int class_id) {
+      @RequestParam(value = "class_id") int classId) {
 
     SchoolClass schoolClass = null;
     Student student = null;
     try {
-      schoolClass = schoolClassService.findById(class_id);
+      schoolClass = schoolClassService.findById(classId);
       student = studentService.findById(id);
 
       student.setSchoolClass(schoolClass);
