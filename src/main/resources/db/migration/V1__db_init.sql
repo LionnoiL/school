6205ca727096e82066a8ba7b1fed4bf9b78a625c
Andriy Gaponov <wmconstructor@gmail.com>
2023-06-02 12:03:07 +0300

    migrate on mysql

--- src/main/resources/db/migration/V1__db_init.sql
+++ src/main/resources/db/migration/V1__db_init.sql
@@ -1,33 +1,33 @@
-CREATE TABLE public.school (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE school (
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	name varchar(100) NOT NULL,
 	description varchar(500) NULL,
 	address varchar(250) NULL,
 	CONSTRAINT school_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE public.academic_year (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE academic_year (
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	name varchar(50) NOT NULL,
 	start_year date NOT NULL,
 	end_year date NOT NULL,
-	school_id long NOT NULL,
+	school_id BIGINT NOT NULL,
 	CONSTRAINT academic_year_pkey PRIMARY KEY (id),
-	CONSTRAINT year_fk_school FOREIGN KEY (school_id) REFERENCES public.school(id)
+	CONSTRAINT year_fk_school FOREIGN KEY (school_id) REFERENCES school(id)
 );
 
-CREATE TABLE public.academic_term (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
-	academic_year_id long NOT NULL,
+CREATE TABLE academic_term (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	academic_year_id BIGINT NOT NULL,
 	start_term date NOT NULL,
 	end_term date NOT NULL,
 	name varchar(50) NOT NULL,
 	CONSTRAINT academic_term_pkey PRIMARY KEY (id),
-	CONSTRAINT academic_term_fk_academic_year FOREIGN KEY (academic_year_id) REFERENCES public.academic_year(id)
+	CONSTRAINT academic_term_fk_academic_year FOREIGN KEY (academic_year_id) REFERENCES academic_year(id)
 );
 
-CREATE TABLE public.users (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE users (
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	username varchar(45) NOT NULL,
 	password varchar(64) NOT NULL,
 	role varchar(45) NOT NULL,
@@ -35,57 +35,56 @@ CREATE TABLE public.users (
   is_parent bool NULL,
 	CONSTRAINT users_pkey PRIMARY KEY (id)
 );
-CREATE UNIQUE INDEX users_username_idx ON public.users USING btree (username);
 
-CREATE TABLE public.grades (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE grades (
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	name varchar(3) NOT NULL,
 	rate numeric(5,2) NOT NULL,
 	CONSTRAINT grades_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE public.courses (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE courses (
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	name varchar(100) NOT NULL,
 	active bool NULL,
 	description varchar(500) NULL,
 	CONSTRAINT courses_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE public.school_class (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE school_class (
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	name varchar(50) NULL,
 	description varchar(500) NULL,
-	school_id long NULL,
+	school_id BIGINT NULL,
 	CONSTRAINT school_class_pkey PRIMARY KEY (id),
-	CONSTRAINT school_clas_fk_school FOREIGN KEY (school_id) REFERENCES public.school(id)
+	CONSTRAINT school_clas_fk_school FOREIGN KEY (school_id) REFERENCES school(id)
 );
 
-CREATE TABLE public.students (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE students (
+	id BIGINT NOT NULL AUTO_INCREMENT,
 	first_name varchar(45) NOT NULL,
 	last_name varchar(45) NOT NULL,
-	school_class_id long NOT NULL,
+	school_class_id BIGINT NOT NULL,
 	CONSTRAINT pupils_pkey PRIMARY KEY (id),
-	CONSTRAINT pupils_fk_school_class FOREIGN KEY (school_class_id) REFERENCES public.school_class(id)
+	CONSTRAINT pupils_fk_school_class FOREIGN KEY (school_class_id) REFERENCES school_class(id)
 );
 
-CREATE TABLE public.class_courses (
-	class_id long NOT NULL,
-	courses_id long NOT NULL,
+CREATE TABLE class_courses (
+	class_id BIGINT NOT NULL,
+	courses_id BIGINT NOT NULL,
 	CONSTRAINT class_courses_pkey PRIMARY KEY (class_id, courses_id),
-	CONSTRAINT class_courses_fk_class FOREIGN KEY (class_id) REFERENCES public.school_class(id),
-	CONSTRAINT class_courses_fk_corses FOREIGN KEY (courses_id) REFERENCES public.courses(id)
+	CONSTRAINT class_courses_fk_class FOREIGN KEY (class_id) REFERENCES school_class(id),
+	CONSTRAINT class_courses_fk_corses FOREIGN KEY (courses_id) REFERENCES courses(id)
 );
 
-CREATE TABLE public.students_grades (
-	id long NOT NULL GENERATED ALWAYS AS IDENTITY,
-	student_id long NOT NULL,
-	course_id long NOT NULL,
-	grade long NOT NULL,
+CREATE TABLE students_grades (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	student_id BIGINT NOT NULL,
+	course_id BIGINT NOT NULL,
+	grade BIGINT NOT NULL,
 	grade_date date NOT NULL,
 	CONSTRAINT students_grades_pkey PRIMARY KEY (id),
-	CONSTRAINT grades_student_id_fk_students FOREIGN KEY (student_id) REFERENCES public.students(id),
-	CONSTRAINT students_grades_course_id_fkey FOREIGN KEY (course_id) REFERENCES public.courses(id)
+	CONSTRAINT grades_student_id_fk_students FOREIGN KEY (student_id) REFERENCES students(id),
+	CONSTRAINT students_grades_course_id_fkey FOREIGN KEY (course_id) REFERENCES courses(id)
 );
 
--- src/main/resources/db/migration/V2__add_admin_user.sql
+++ src/main/resources/db/migration/V2__add_admin_user.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.users(
+INSERT INTO users(
 	username, password, role, enabled, is_parent)
 	VALUES ('admin', '$2a$12$GlMm3HFiZJzVLlW0XlnfWefAskwOJnUEWyfIwuxEk8HzPU2qoOJD.', 'ROLE_ADMIN', true, true);
